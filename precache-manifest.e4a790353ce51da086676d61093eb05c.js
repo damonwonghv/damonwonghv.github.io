c025396d1f524d4e1fc82e03fbf17900c359d090
Damon Wong <damonwonghv@users.noreply.github.com>
2020-11-27 13:48:34 +0800

    Initial

---
+++ precache-manifest.e4a790353ce51da086676d61093eb05c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe10b3ab438d030f8059",
+    "url": "css/app.c6522462.css"
+  },
+  {
+    "revision": "29978728f1b279f531c0",
+    "url": "css/chunk-vendors.bc34dfbd.css"
+  },
+  {
+    "revision": "3d1f8fa2f06249540889a7bbe69cf5bb",
+    "url": "fonts/materialdesignicons-webfont.3d1f8fa2.eot"
+  },
+  {
+    "revision": "3e722fd57a6db80ee119f0e2c230ccff",
+    "url": "fonts/materialdesignicons-webfont.3e722fd5.ttf"
+  },
+  {
+    "revision": "4187121a4353440c2a865dbf1bc1901b",
+    "url": "fonts/materialdesignicons-webfont.4187121a.woff2"
+  },
+  {
+    "revision": "fec1b66adcf131415a2511bd77e747cc",
+    "url": "fonts/materialdesignicons-webfont.fec1b66a.woff"
+  },
+  {
+    "revision": "6da1d457e23e707a7ed543fb8f73e301",
+    "url": "index.html"
+  },
+  {
+    "revision": "fe10b3ab438d030f8059",
+    "url": "js/app.3d4175be.js"
+  },
+  {
+    "revision": "29978728f1b279f531c0",
+    "url": "js/chunk-vendors.1cd1fa0c.js"
+  },
+  {
+    "revision": "51a02e875661c86ed87187eea3e053d4",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,39 @@
+importScripts("precache-manifest.e4a790353ce51da086676d61093eb05c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// Inside of webpack.config.js:
+/*
+const {GenerateSW} = require('workbox-webpack-plugin');
+
+module.exports = {
+  // Other webpack config...
+  plugins: [
+    // Other plugins...
+    new GenerateSW({
+      option: 'value',
+    })
+  ]
+};
+*/
+// TODO: Install workBox-build from a command prompt
+// TODO:   npm install workbox-build
+const workboxBuild = require('workbox-build');
+const SRC_DIR = 'src';
+const BUILD_DIR = '{1:dist}';
+const SW = 'sw.js';
+
+const input ={
+  swSrc: `${SRC_DIR}/${SW}`,
+  swDest: `${BUILD_DIR}/${SW}`,
+  globDirectory: BUILD_DIR,
+  globPatterns: [
+    '**/*.{js,png,ico,svg,html,css}',
+    'assets/**/*'
+  ],
+  globIgnores: ['sw.js'],
+  maximumFileSizeToCacheInBytes: 4000000
+};
+
+workboxBuild.injectManifest(input).then(() =>{
+  console.log(`The service worker ${BUILD_DIR}/${SW} has been injected with a precache list.`);
+});
+
